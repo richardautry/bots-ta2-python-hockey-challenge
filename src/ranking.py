339383aa65e8ce735993fa26ace19d4a199b9795
Richard Autry <autry.richard@gmail.com>
2022-09-11 11:32:54 -0400

    Current methods generate expected output

--- src/ranking.py
+++ src/ranking.py
@@ -1,4 +1,88 @@
+from enum import Enum
+from typing import Dict
+
+
+class MatchResult(Enum):
+    """
+    Possible outcome labels for a match
+    """
+    WIN = 1
+    TIE = 2
+    LOSS = 3
+
+
+# Default score map for assigning scores to match results
+SCORE_MAP = {
+    MatchResult.WIN: 3,
+    MatchResult.TIE: 1,
+    MatchResult.LOSS: 0
+}
+
+
+def get_match_result(team_name_a: str, score_a: int, team_name_b: str, score_b: int) -> Dict[str, MatchResult]:
+    """
+    Given two teams and corresponding scores, return the match results
+    as dict of { team_name: MatchResult }
+
+    Args:
+        team_name_a: Team A's name
+        score_a: Team A's score in the match
+        team_name_b: Team B's name
+        score_b: Team B's score in the match
+
+    Returns: Results Dict
+
+    """
+    if score_a > score_b:  # Team A won
+        result = {
+            team_name_a: MatchResult.WIN,
+            team_name_b: MatchResult.LOSS
+        }
+    elif score_b > score_a:  # Team B won
+        result = {
+            team_name_a: MatchResult.LOSS,
+            team_name_b: MatchResult.WIN
+        }
+    else:  # Tie
+        result = {
+            team_name_a: MatchResult.TIE,
+            team_name_b: MatchResult.TIE
+        }
+    return result
+
+
+def get_score(match_result: MatchResult, score_map: Dict[MatchResult, int] = None) -> int:
+    """
+    Get a score for ranking teams per MatchResult
+    Uses default score_map if none given
+
+    Args:
+        match_result: MatchResult
+        score_map: Dict of { MatchResult: int } to assign scores
+
+    Returns: Assigned score as int
+    """
+    if not score_map:
+        score_map = SCORE_MAP
+    return score_map[match_result]
+
 
 class Ranking:
-    # TODO: Add your code here
-    pass
+    def __init__(self, team_name: str):
+        self.team_name = team_name
+        self._score = 0
+        self.rank = 1
+
+    @property
+    def score(self):
+        return self._score
+
+    def add_score(self, match_result: MatchResult):
+        self._score += get_score(match_result)
+
+    def __str__(self):
+        return f"{self.team_name}: {self.score}"
+
+    def __gt__(self, other):
+        # TODO: Possibly use lambda instead, also sort by alpha
+        return self.score > other.score
--- task.py
+++ task.py
@@ -1,6 +1,27 @@
 import argparse
+import csv
+from typing import List, Dict, Union
 
-from src.ranking import Ranking
+from src.ranking import Ranking, get_match_result
+
+
+def parse_input_row(input_row: List[str]) -> Dict[str, Union[str, int]]:
+    """
+    Parse csv input row according to required arguments in `ranking.get_match_result`
+
+    Args:
+        input_row: csv input row as List of strs
+
+    Returns: Keyword arguments for ranking.get_match_result as dict
+    """
+    team_a_split = input_row[0].split()
+    team_b_split = input_row[1].split()
+    return {
+        "team_name_a": " ".join(team_a_split[:-1]),
+        "score_a": int(team_a_split[-1]),
+        "team_name_b": " ".join(team_b_split[:-1]),
+        "score_b": int(team_b_split[-1])
+    }
 
 
 def entrypoint() -> None:
@@ -24,9 +45,45 @@ def entrypoint() -> None:
     output_file = args.output_file
 
     # TODO: Add your code here
-    # (you may modify the line below)
-    my_ranking = Ranking()
+    all_rankings = dict()
+    with open(input_file) as file:
+        file_reader = csv.reader(file)
+        for row in file_reader:
+            if file_reader.line_num > 1:  # Skip header
+                parsed_args = parse_input_row(row)
+                team_name_a = parsed_args["team_name_a"]
+                team_name_b = parsed_args["team_name_b"]
+                if not all_rankings.get(team_name_a):
+                    all_rankings[team_name_a] = Ranking(team_name_a)
 
+                if not all_rankings.get(team_name_b):
+                    all_rankings[team_name_b] = Ranking(team_name_b)
+
+                match_results = get_match_result(**parsed_args)
+
+                all_rankings[team_name_a].add_score(match_results[team_name_a])
+                all_rankings[team_name_b].add_score(match_results[team_name_b])
+
+    with open(output_file, 'w') as file:
+        file_writer = csv.writer(file)
+        # Write headers
+        file_writer.writerow(["Place", "Team", "Score"])
+
+        current_rank = 1
+        prev_ranking = None
+        for ranking in sorted(list(all_rankings.values()), reverse=True):
+            if prev_ranking and prev_ranking.score == ranking.score:
+                ranking.rank = prev_ranking.rank
+            else:
+                ranking.rank = current_rank
+
+            ranking_score_unit = "pts"
+            if ranking.score == 1:
+                ranking_score_unit = "pt"
+
+            file_writer.writerow([ranking.rank, ranking.team_name, f"{ranking.score} {ranking_score_unit}"])
+            prev_ranking = ranking
+            current_rank += 1
 
 if __name__ == "__main__":
     entrypoint()
